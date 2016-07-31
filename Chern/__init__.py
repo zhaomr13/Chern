751bc1f6c94b90075cba64edef0e051d1dfe5c84
mzhao <mingrui.zhao@cern.ch>
2016-07-31 23:52:28 +0800

    modify git __init__

--- Chern/__init__.py
+++ Chern/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-from .import_file import import_file
-from .compile_file import compile_file
-from .call_file import call
+#from .import_file import import_file
+#from .compile_file import compile_file
+#from .call_file import call
+import projects
 
-__author__ = 'TUHEP'
-__version__ = ''
+__author__ = 'Mingrui Zhao'
+__version__ = '3'
 __version_info = ()
 __revision__ = ''
 __licensse__ = ''
