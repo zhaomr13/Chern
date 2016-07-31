5f24ba30da6b289930b4c06ae4ceb8c10c70f625
mzhao <mingrui.zhao@cern.ch>
2016-07-31 23:55:32 +0800

    task class

---
+++ Chern/task.py
@@ -0,0 +1,15 @@
+class task:
+    def __init__(self, name, algorithm)():
+        self.name = name
+        self.algorithm = algorithm
+    parents = []
+    comment = ""
+    input_files = ""
+    output_files = ""
+
+    def register() :
+        # save the configuration
+
+    def start_binary()
+
+
---
+++ Chern/utils.py
@@ -0,0 +1,38 @@
+# Load module
+import os
+import sys
+
+def read_variables(module_name, path):
+    from imp import load_source
+    module = load_source(module_name, path)
+    return module
+
+
+def write_variables(module, path, variables):
+    f = open(path, "write")
+
+    # Module variables, key value mapping
+    dic = {key:value for key,value in module.__dict__.iteritems()}
+
+    # Add new variables to the list
+    old_variables = dir(module)
+    for key, value in variables:
+        if key not in old_variables:
+            old_variables.append(key)
+
+    # Change values
+    for key, value in variables:
+        dic[key] = value
+
+    # Save to file
+    print old_variables
+    for key in old_variables:
+        if not key.startswith("__") and not key.startswith("-") :
+            if type(dic[key]) == str :
+                f.write("%s='%s'\n"%(key, str(dic[key])) )
+            else :
+                f.write("%s=%s\n"%(key, str(dic[key])) )
+
+
+#c = read_variables("configuration", os.environ["HOME"]+"/.Chern/configuration.py")
+#write_variables(c, os.environ["HOME"]+"/.Chern/configuration.py", [("hello", [3124567])])
