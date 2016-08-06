a5373fc24bcace6f83aec65846c9e1ad3fb0240e
mzhao <mingrui.zhao@cern.ch>
2016-08-06 19:17:31 +0800

    add more details

--- Chern/server.py
+++ Chern/server.py
@@ -1,17 +1,84 @@
 import os
 import Chern as chen
 
+# global configurations
 global_config_path = os.environ["HOME"] + "/.Chern"
-if os.path.exists(global_config_path+"/server/lock") :
-    print "Something is wrong because of server"
-    exit(0)
 
-print "server started ..."
-while not os.path.exists(global_config_path+"/server/close") :
-    config = utils.read_variables("configuration", global_config_path+"/configuration.py")
-    projects_list = config.projects_list
-    projects_path = config.projects_path
+# Why do I need such a lock?
+# if os.path.exists(global_config_path+"/server/lock") :
+# print "Something is wrong because of server"
+# exit(0)
+
+print "server started."
+# Setup a running tasks list for every project
+running_jobs = {}
+
+# Data structure for saving running tasks status
+from collections import namedtuple
+task_type = namedtuple("task_type", ["pid", "name", "poll_status", "previous_node", "next_node"])
+
+class running_jobs_list:
+    def __init__():
+        self.size = 0
+        self.tail = None
+
+    def append(pid, name):
+        new_node = task_type(pid, name, None, next_node, tail, None)
+        if self.tail is not None:
+            self.tail.next_node = new_node
+        self.tail = new_node
+        self.size += 1
+
+    def remove():
+        remove_list = []
+        present = self.tail
+        while present is not None:
+            present.poll_status = present.pid.poll()
+            if present.poll_status is not None:
+                remove_list.append(present)
+                if present.next_node is not None:
+                    present.next_node.previous_node = present.previous_node
+                if present.previous_node is not None:
+                    present.previous_node.next_node = present.new_node
+                if present is self.tail:
+                    self.tail = present.previous_node
+                size -= 1
+            present = present.previous_node
+        return remove_list
+
+
+# Loop forever to start applications
+while not os.path.exists(global_config_path+"/server/lock") :
+
+    # Get the global configurations
+    global_config = utils.read_variables("configuration", global_config_path+"/configuration.py")
+    projects_list = global_config.projects_list
+    projects_path = global_config.projects_path
+
+    # Loop over all the projects
     for project in projects_list:
+        # Create new list for new project
+        if not project in running_jobs:
+            running_jobs[project] = running_jobs_list()
+        # Remove the finished jobs and change the status according to the return value
+        finished_list = running_jobs[project].remove()
+        for t in finished_list:
+            rest_ncpus += t.ncpus
+            change_task_status(project, t.name, t.poll_status)
+
+        # Add new jobs according to cpus
+        tasks_name_list = get_tasks_name_list(project, status == "new")
+        for task_name in tasks_name_list:
+            t = task(task_name, project)
+            if not t.check_start(rest_ncpus):
+                continue
+            rest_ncpus -= t.ncpus
+            ps = t.start()
+            running_jobs[project].append(ps, t.name)
+
+
+
+        """
         c = utils.read_variables("tasks", projects_path[project])
         #ncpus = c.ncpus
         for index, ps in enumerate(running_jobs):
@@ -23,7 +90,10 @@ while not os.path.exists(global_config_path+"/server/close") :
         for tasks in tasks_list:
             # tryto start tasks
             task = task(name)
+            change_task_status(project, task_name, "starting")
             task.start()
+            change_task_status(project, task_name, "running")
+        """
 
     # check jobs
     # check the job list
@@ -34,3 +104,4 @@ while not os.path.exists(global_config_path+"/server/close") :
 
 
 open(global_config_path+"/server/close", "a").close()
+print "server closed."
