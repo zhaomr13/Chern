4bb0228f2aa4aaa16c941b63d921df8f1bc6b6be
zhaomr <mingrui zhao>
2017-07-09 10:45:38 +0800

    modify startup message

--- profile/profile_chern/startup/10startup_message.py
+++ profile/profile_chern/startup/10startup_message.py
@@ -1,2 +1,6 @@
-print("Chern: A data analysis framework")
-print("by Mingrui Zhao @ Center of High Energy Physics, Tsinghua University")
+if True:
+    print("Chern: A data analysis framework")
+    print("Author: Mingrui Zhao, dedicated to Sidan")
+    print("@ Center of High Energy Physics, Tsinghua University")
+    print("@ Department of Nuclear Physics, China Institute of Atomic Energy")
+    print("Email: mingrui.zhao@mail.labz0.org")
