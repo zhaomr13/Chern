d3f8e8221f220bcb7c41da7ad148bf14edc25d01
zhaomr <mingrui zhao>
2017-07-09 10:36:09 +0800

    update startup message

--- profile/profile_chern/startup/10startup_message.py
+++ profile/profile_chern/startup/10startup_message.py
@@ -1,1 +1,2 @@
-print("Chern: A data analysis framework by Mingrui Zhao @ THU")
+print("Chern: A data analysis framework")
+print("by Mingrui Zhao @ Center of High Energy Physics, Tsinghua University")
