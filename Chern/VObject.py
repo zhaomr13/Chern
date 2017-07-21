05f88bd78fcb8dd969731b63351156aecc41c4df
zhaomr <mingrui zhao>
2017-07-21 17:33:29 +0800

    change behavior of alias

--- Chern/VObject.py
+++ Chern/VObject.py
@@ -68,6 +68,7 @@ class VObject(object):
         can be used
         """
         debug("Running ls")
+        print(self.readme())
         sub_objects = self.sub_objects()
         print(sub_objects)
         sub_objects.sort(key=lambda x:(x.object_type(),x.path))
@@ -133,6 +134,8 @@ class VObject(object):
     def get_successors(self):
         config_file = utils.ConfigFile(self.path+"/.config.py")
         succ_str = config_file.read_variable("successors")
+        if succ_str is None:
+            return []
         successors = []
         for path in succ_str:
             successors.append(path)
@@ -141,6 +144,8 @@ class VObject(object):
     def get_predecessors(self):
         config_file = utils.ConfigFile(self.path+"/.config.py")
         pred_str = config_file.read_variable("predecessors")
+        if pred_str is None:
+            return []
         predecessors = []
         for path in pred_str:
             predecessors.append(path)
@@ -150,34 +155,83 @@ class VObject(object):
         """
         FIXME
         """
-        queue = self.sub_objects_respectively()
+        queue = self.sub_objects_recursively()
         for obj in queue:
             new_object = VObject(new_path +"/"+ self.relative_path(obj.path))
-            for pred_object in obj.get_predecessors():
-                # if in the same tree
-                if self.relative_path(pred_object.path).startwith(".."):
-                    new_object.add_in_arc(pred_object.path)
-                else:
-                    relative_path = self.relative_path(pred_object.path)
-                    new_object.add_in_arc(new_path+"/"+relative_path)
-            for succ_object in obj.get_successors():
-                if self.relative_path(succ_object.path).startwith(".."):
-                    new_object.add_out_arc(succ_object.path)
+
+    def path_to_alias(self, path):
+        config_file = utils.ConfigFile(self.path+"/.config.py")
+        path_to_alias = config_file.read_variable("path_to_alias")
+        return path_to_alias[path]
+
+    def alias_to_path(self, path):
+        config_file = utils.ConfigFile(self.path+"/.config.py")
+        alias_to_path = config_file.read_variable("alias_to_path")
+        return path_to_alias[path]
+
+    def remove_alias(self, alias):
+        config_file = utils.ConfigFile(self.path+"/.config.py")
+        alias_to_path = config_file.read_variable("alias_to_path")
+        path_to_alias = config_file.read_variable("path_to_alias")
+        path = alias_to_path[alias]
+        path_to_alias.pop(path)
+        alias_to_path.pop(alias)
+        config_file.write_variable("alias_to_path", alias_to_path)
+        config_file.write_variable("path_to_alias", path_to_alias)
+
+
+
+    def set_alias(self, path, alias):
+        config_file = utils.ConfigFile(self.path+"/.config.py")
+        path_to_alias = config_file.read_variable("path_to_alias")
+        alias_to_path = config_file.read_variable("alias_to_path")
+        path_to_alias[path] = alias
+        alias_to_path[alias] = path
+        config_file.write_variable("path_to_alias", path_to_alias)
+        config_file.write_variable("alias_to_path", alias_to_path)
+
+
 
     def mv(self, new_path):
         """
         FIXME
         mv to another path
         """
-        self.cp(new_path)
+        queue = self.sub_objects_recursively()
+        for obj in queue:
+            new_object = VObject(new_path +"/"+ self.relative_path(obj.path))
+            for pred_object in obj.get_predecessors():
+                if self.relative_path(pred_object.path).startwith(".."):
+                    new_object.add_in_arc(pred_object.path)
+                    alias1 = obj.path_to_alias(pred_object.path)
+                    alias2 = pred_object.path_to_alias(obj.path)
+                    new_object.set_alias(alias1, pred_object.path)
+                    pred_object.set_alias(alias2, new_object.path)
+                else:
+                # if in the same tree
+                    relative_path = self.relative_path(pred_object.path)
+                    new_object.add_in_arc(new_path+"/"+relative_path)
+                    alias1 = obj.path_to_alias(pred_obj.path)
+                    alias2 = pred_obj.path_to_alias(obj.path)
+                    new_object.set_alias(alias1, new_path+"/"+relative_path)
+                    VObject(new_path+"/"+relative_path).set_alias(alias2, new_object.path)
+            for succ_object in obj.get_successors():
+                if self.relative_path(succ_object.path).startwith(".."):
+                    new_object.add_out_arc(succ_object.path)
+                    alias1 = obj.path_to_alias(succ_object.path)
+                    alias2 = succ_object.path_to_alias(obj.path)
+                    new_object.set_alias(alias1, succ_object.path)
+                    succ_object.set_alias(alias2, new_object.path)
+
         self.remove()
         pass
 
     def remove(self):
         """
-
+        Remove this object.
+        The important this is to unalias
         """
-        queue = self.sub_objects_respectively()
+        queue = self.sub_objects_recursively()
         for obj in queue:
             for pred_object in obj.get_predecessors():
                 if self.relative_path(pred_object.path).startwith(".."):
