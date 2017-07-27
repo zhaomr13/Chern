00411334bf4526fc00283464afd0523ce662a5af
zhaomr <mingrui zhao>
2017-07-27 16:03:06 +0800

    modify according to pylint

--- Chern/VDirectory.py
+++ Chern/VDirectory.py
@@ -1,13 +1,9 @@
+"""
+A module
+"""
 from Chern.VObject import VObject
 class VDirectory(VObject):
-    def __init__(self, file_name, parent):
-        super(VDirectory, self).__init__(file_name, parent)
-        with open(self.path+"/.type", "w") as type_file:
-            type_file.write("Directory")
-
-    def load_object(self):
-        super(VDirectory, self).load_object()
-
-    def mk_data(self, file_name):
-        pass
-
+    """
+    Nothing more to do for this VDirectory.
+    """
+    pass
