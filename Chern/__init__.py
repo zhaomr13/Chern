9246424c66ea0de2c10afe1cecde77062e7311a1
zhaomr <mingrui zhao>
2017-07-27 16:04:43 +0800

    update according to pylint

--- Chern/__init__.py
+++ Chern/__init__.py
@@ -2,12 +2,8 @@
 Chern
 """
 from Chern import utils
-from Chern.VObject import VObject
-from Chern.VAlgorithm import VAlgorithm #as _VAlgorithm
-from Chern.VTask import VTask #as _VTask
-from Chern.VData import VData #as _VData
-from Chern.VDirectory import VDirectory
-from Chern.VProject import VProject
+from Chern.shell import mv, cd
+from Chern.shell import mkdir, mktask, mkdata, mkalgorithm
 
 # VObject.sub_types = {"Algorithm":VAlgorithm, "Task":VTask, "Data":VData, "Directory":VDirectory}
 __author__ = 'Mingrui Zhao'
@@ -16,20 +12,5 @@ __version_info = ()
 __revision__ = ''
 __license__ = ''
 __date__ = ''
-
-def create_object_instance(path):
-    """ Create an object instance
-    """
-    path = utils.strip_path_string(path)
-    object_config_file = utils.ConfigFile(path+"/.config.py")
-    object_type = object_config_file.read_variable("object_type")
-    vobject_class = {"algorithm":VAlgorithm,
-                     "task":VTask,
-                     "data":VData,
-                     "directory":VDirectory,
-                     "project":VProject}
-    return vobject_class[object_type](path)
-
-
 
 
