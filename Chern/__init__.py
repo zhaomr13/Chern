8a99eca9475736c88ab2f3a61f6144c064508939
zhaomr <mingrui zhao>
2017-08-31 22:42:34 +0800

    update __init__

--- Chern/__init__.py
+++ Chern/__init__.py
@@ -7,7 +7,7 @@ from Chern.shell import mkdir, mktask, mkdata, mkalgorithm
 
 # VObject.sub_types = {"Algorithm":VAlgorithm, "Task":VTask, "Data":VData, "Directory":VDirectory}
 __author__ = 'Mingrui Zhao'
-__version__ = '3'
+__version__ = 'v1r0p0'
 __version_info = ()
 __revision__ = ''
 __license__ = ''
