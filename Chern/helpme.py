62f46a15196f4cb25351cf218ce66a5a4523094d
zhaomr <mingrui zhao>
2017-10-24 18:57:56 +0800

    update helpme

---
+++ Chern/helpme.py
@@ -0,0 +1,11 @@
+helpme_func = {}
+
+def helpme_hello():
+    print("Hello")
+helpme_func["hello"] = helpme_hello
+
+def helpme_new():
+    print("""If you are new to the program, please contact
+If any bugs is found in the program, please contact mingrui.zhao@mail.labz0.org""")
+helpme_func["new"] = helpme_new
+
