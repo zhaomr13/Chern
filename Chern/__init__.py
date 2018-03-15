c5572ce0968b2c1563be03c72f9e7a89ad11494a
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-03-15 16:50:57 +0800

    A brand new version

--- Chern/__init__.py
+++ Chern/__init__.py
@@ -10,7 +10,7 @@ from Chern.main import main
 # here = os.path.abspath(os.path.dirname(__file__) + "/../profile_chern")
 
 __author__ = 'Mingrui Zhao'
-__version__ = 'v1r0p0'
+__version__ = '2.0.0'
 __version_info = ()
 __revision__ = ''
 __license__ = ''
