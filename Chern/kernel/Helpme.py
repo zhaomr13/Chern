7367f769cd06e3e14bbd337d203ea7d1939411c9
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-13 15:29:58 +0800

    Write more helpme

--- Chern/kernel/Helpme.py
+++ Chern/kernel/Helpme.py
@@ -1,10 +1,21 @@
-project_helpme = { "": """Hello, you are in the VProject object, what would you like to do?
+project_helpme = { "": """
+Hello, you are in a ``project''. What would you like to do?
 You can consult http://chern.readthedocs.io/en/latest/ for a complete document.
-The commands to use:
-    cd [object]
-    ls : list the containings
-    mktask :
-    mkalgorithm : """,
+
+The following commands can be used, and type ``helpme [COMMAND]'' or ``[COMMAND] --help'' to see the help in detail:
+    cd           : Change to directory/object.
+    ls           : List the containings.
+    mv           : Move a directory/object to another place.
+    rm           : Remove a directory/object.
+    mkdir        : Create a directory.
+    mk_task      : Create a task.
+    mk_algorithm : Create an algorithm.
+    --------------------------------
+    status       : Show the status of the objects.
+    --------------------------------
+    ls_project   : List the projects.
+    cd_project   : Switch to another project.
+""",
 
 "cd": """The usage of cd:
     cd [object] """,
@@ -12,14 +23,28 @@ The commands to use:
 "cd test": """Test for cd"""
                   }
 
-task_helpme = {"": """Hello, you are in a VTask object. What would you like to do?
-You can use the following commands:
-    add input [VTASK] [ALIAS]
-    add parameter [PARAMETER] [VALUE]
-    add output [FILENAME]
-    impress
-    submit
-    readme """,
+task_helpme = {"": """
+Hello, you are now operating a ``task''. What would you like to do?
+
+The following commands can be used, and type ``helpme [COMMAND]'' or ``[COMMAND] --help'' to see the help in detail:
+    ls               : List the containings
+    la               : A short
+    add_source       : Add source to a task. The source should be a directory and it will be regarded the output of the current task.
+    add_input        : Add input to a task. The input should also be a task.
+    add_algorithm    : Add algorithm to the current task. The input should also be a task.
+    add_parameter    : Add a parameter to the
+    remove_source    : This command does not exists.
+    remove_input     : Add input to a task. The input should also be a task.
+    remove_algorithm : Add algorithm to the current task. The input should also be a task.
+    remove_parameter : Add a parameter to the
+    --------------------------------
+    status           : Get the status of the objects.
+    impress          : Create a new impression for the task.
+    submit           : Submit the impression to the backend.
+    jobs             : Consult the jobs.
+    stdout           : Get the stdout.
+    readme           : Read or write the readme.
+""",
 "status": """The status of a task may be one of the following:
     new: The task is edited and not impressed yet.
     impressed: The task is impressed, however, there is not a job link to the impression.
@@ -30,7 +55,7 @@ You can use the following commands:
     missing: """
 }
 
-algorithm_helpme = { "": """Hello, you are in the VProject object, what would you like to do?
+algorithm_helpme = { "": """Hello, you are operating an ``algorithm'', what would you like to do?
 The commands to use:
     cd [object]
     ls : list the containings
@@ -43,9 +68,9 @@ The commands to use:
 "cd test": """Test for cd"""
                   }
 
-directory_helpme = { "": """Hello, you are in the VProject object, what would you like to do?
+directory_helpme = { "": """Hello, you are in the ``directory'' object, what would you like to do?
 The commands to use:
-    cd [object]
+    cd : change directory
     ls : list the containings
     mktask :
     mkalgorithm : """,
