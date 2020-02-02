18ae3f18032ebe95598fa838a35362aa07eb0ab2
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2020-02-02 16:45:24 +0100

    add more description

--- Chern/kernel/VObject.py
+++ Chern/kernel/VObject.py
@@ -1,4 +1,66 @@
-""" Virtual class for all ```directory'', ``task'', ``algorithm''
+""" Virtual base class for all ```directory'', ``task'', ``algorithm''
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    VObject:
+    #Methods:
+        + __init__:
+            It should be initialized with a absolute path. The initialization gets variable "path" and "config_file".
+            It is quite light-weight to create a VObject. A VObject can be abandoned after using and create another when needed.
+
+        + __str__, __repr__:
+            For print
+
+        + invariant_path:
+            Return the path relative to the project root
+        + relative_path(a_path);
+            Return the path of the VObject relative to a_path
+
+        + object_type:
+            Return the type of the object: project, task, directory, or empty("")
+        + is_zombine
+            To judge whether this object has a type
+
+        + color_tag: (Maybe put it somewhere else?)
+            For print
+
+        + ls:
+            Realization of "ls" call o.ls() will give you the contents of the object
+
+        + add_arc_from(obj)
+        + remove_arc_from(obj, single=False)
+        + add_arc_to(obj)
+        + remove_arc_from(obj, single=False):
+            Add or remove arcs, the "single" variable is only used for debug usage.
+
+        + successors
+        + predecessors:
+            Get [objects]
+        + has_successor(obj)
+        + has_predessor(obj)
+            Judge whether obj is the succ/pred of this obj
+
+        + doctor:
+            Judge whether it is a good object(with all arc linked)
+
+        + copy_to:
+            Copy the object and its contains to a new path. Before the copy, all objects in the directory will be impressed.
+            The arcs within the directory will be kept and outsides will be removed.
+        + clean_impressions
+        + clean_flow:
+            Helpers for copy_to
+
+        + path_to_alias
+        + alias_to_path
+        + has_alias
+        + set_alias
+        + remove_alias
+
+        + move_to:
+            Move the object to another directory
+
+        + add: FIXME what is it?
+
+
+
 """
 import os
 import shutil
@@ -22,7 +84,7 @@ class VObject(object):
 
     def __init__(self, path):
         """ Initialize a instance of the object.
-        All the infomation is directly read from and write to the disk.
+        All the information is directly read from and write to the disk.
         parameter ``path'' is allowed to be a string begin with empty characters.
         """
         self.path = csys.strip_path_string(path)
